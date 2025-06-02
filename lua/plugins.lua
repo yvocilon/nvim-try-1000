587acae2aa9cf1a2d4bc9504fb907ebaa23b7a18
Yvo Cilon <yvo.cilon@appcore.nl>
2025-06-02 15:17:26 +0200

    Add lazy loading for neo-tree plugin and set command to trigger it

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -106,6 +106,8 @@ return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
+		lazy = true,
+		cmd = "Neotree",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
